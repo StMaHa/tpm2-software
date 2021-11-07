801e3c1df3d32798daced39ffa24f12cb8a8f055
Stefan Mahnke-Hartmann <12825445+StMaHa@users.noreply.github.com>
2021-11-07 12:21:18 +0100

    Add installation steps adopted from  "OPTIGA TPM Application Note"

--- setup-abrmd.sh
+++ setup-abrmd.sh
@@ -1,20 +1,36 @@
-echo "Setup TPM2 Access Broker and Resource Manager"
+echo "*** Setup TPM2 Access Broker and Resource Manager ***"
+echo "Installation of packages required to build the TABRMD"
 sudo apt -y install \
    libdbus-1-dev \
    libglib2.0-dev
+
+echo "Change to the installation directory and download the TABRMD source code"
 cd /home/pi/tssSetup
 git clone https://github.com/tpm2-software/tpm2-abrmd
+
+echo "Change to the source code directory and bootstrap the TABRMD"
 cd tpm2-abrmd
 ./bootstrap
+
+echo "Configuring the TABRMD"
 ./configure --with-dbuspolicydir=/etc/dbus-1/system.d \
             --with-systemdsystemunitdir=/lib/systemd/system \
             --with-systemdpresetdir=/lib/systemd/system-preset \
             --datarootdir=/usr/share
+
+echo "Building and installing the TABRMD"
 make -j4
 sudo make install
 
+echo "Post-installation steps for TABRMD"
 sudo ldconfig
 sudo pkill -HUP dbus-daemon
 sudo systemctl daemon-reload
 sudo systemctl enable tpm2-abrmd.service
 sudo systemctl start tpm2-abrmd.service
+cd /home/pi/tssSetup
+
+echo " Sanity Tests"
+ dbus-send --system --dest=org.freedesktop.DBus --type=method_call \
+--print-reply /org/freedesktop/DBus org.freedesktop.DBus.ListNames
+
