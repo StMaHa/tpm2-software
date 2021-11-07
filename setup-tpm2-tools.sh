5583f143b281358e76a9c720e3688f4a308662fe
Stefan Mahnke-Hartmann <12825445+StMaHa@users.noreply.github.com>
2021-11-07 12:25:45 +0100

    Add installation steps adopted from "OPTIGA TPM Application Note"

--- setup-tpm2-tools.sh
+++ setup-tpm2-tools.sh
@@ -1,4 +1,9 @@
-echo "Setup TPM2 Tools"
+echo "*** Setup TPM2 Tools ***"
+if [ ! -d "/home/pi/tssSetup" ]; then
+  mkdir /home/pi/tssSetup
+fi
+
+echo "Installation of packages required to build the TPM2 Tools"
 sudo apt -y install \
     autoconf \
     automake \
@@ -11,11 +16,20 @@ sudo apt -y install \
     python-yaml \
     expect
 
+echo "Change to the installation directory and download the TPM2 Tools source code"
+cd /home/pi/tssSetup
+# git clone -b 4.2.X https://github.com/tpm2-software/tpm2-tools.git
 git clone https://github.com/tpm2-software/tpm2-tools
-cd tpm2-tools
+
+echo "Change to the source code directory and bootstrap the TPM2 Tools"
+cd tpm2-tools/
 ./bootstrap
+
+echo "Configuring the TPM2 Tools"
 ./configure
+
+echo "Building and Installing the TPM2 Tools"
 make -j4
 sudo make install
 sudo ldconfig
-cd ..
+cd /home/pi/tssSetup
